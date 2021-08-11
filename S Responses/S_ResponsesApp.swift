9111ded4a47d21a5347d1e4dd8c16879eb50829f
Person <51016337+Person2099@users.noreply.github.com>
2021-08-12 09:29:55 +1000

    Initial Commit

---
+++ S Responses/S_ResponsesApp.swift
@@ -0,0 +1,17 @@
+//
+//  S_ResponsesApp.swift
+//  S Responses
+//
+//  Created by Sebastian Keet on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct S_ResponsesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
