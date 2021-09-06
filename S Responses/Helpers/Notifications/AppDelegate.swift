9a66dec937567af1dbf9fd8ac7a8b63e2a3e2971
Person <51016337+Person2099@users.noreply.github.com>
2021-09-06 13:41:54 +1000

    Added Sentry Error Monitoring

--- S Responses/Helpers/Notifications/AppDelegate.swift
+++ S Responses/Helpers/Notifications/AppDelegate.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import OneSignal
+import Sentry
 
 //MARK: Declare AppDelegate
 // AppDelegate is usually found only is storyboard based applications and not SwiftUI, however it is required by OneSignal (our notification provider) so is declared here
@@ -17,6 +18,13 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 	// Runs upon app launch
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+		SentrySDK.start { options in
+			options.dsn = "https://45e18479122e4c4c84f73d8d4569f982@o297509.ingest.sentry.io/5946719"
+			options.debug = true // Enabled debug when first installing is always helpful
+			options.sessionTrackingIntervalMillis = 60000
+			options.tracesSampleRate = 1.0
+		}
+		
         // Remove this method to stop OneSignal Debugging
         OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
         
--- S ResponsesUITests/S_ResponsesUITests.swift
+++ S ResponsesUITests/S_ResponsesUITests.swift
@@ -20,7 +20,8 @@ class S_ResponsesUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+		continueAfterFailure = false
+	}
 	
 	func testOtherViews() throws {
 		let app = XCUIApplication()
@@ -29,10 +30,8 @@ class S_ResponsesUITests: XCTestCase {
 		let homeButton = app.buttons["Home"]
 		
 		app.buttons["Response List"].tap()
-		app.swipeUp()
-		app.swipeUp()
-		app.swipeDown()
-		app.swipeDown()
+		app.swipeUp(velocity: .fast)
+		app.swipeDown(velocity: .fast)
 		homeButton.tap()
 		
 		app.buttons["Settings"].tap()
@@ -44,13 +43,54 @@ class S_ResponsesUITests: XCTestCase {
 		homeButton.tap()
 	}
 	
-	func testKeyboardButtons() {
+	func testForDetailedDefPlaceholders() throws {
+		
 		let app = XCUIApplication()
 		app.launch()
+		
+		let homeButton = app.buttons["Home"]
+		
+		let key0 = app.keys["0"]
+		let key1 = app.keys["1"]
+		let key2 = app.keys["2"]
+		let key3 = app.keys["3"]
+		let key4 = app.keys["4"]
+		let key5 = app.keys["5"]
+		let key6 = app.keys["6"]
+		let key7 = app.keys["7"]
+		let key8 = app.keys["8"]
+		let key9 = app.keys["9"]
+		
+		let keyDict = ["0": key0, "1": key1, "2": key2, "3": key3, "4": key4, "5": key5, "6": key6, "7": key7, "8": key8, "9": key9]
+		let responseCodes = ["100", "101", "102", "200", "201", "202", "203", "204", "205", "206", "207", "208", "226", "300", "301", "302", "303", "304", "305", "306", "307", "308", "400", "401", "402", "403", "404", "405", "406", "407", "408", "409", "410", "411", "412", "413", "414", "415", "416", "417", "418", "420", "422", "423", "424", "425", "426", "428", "429", "431", "444", "449", "450", "451", "499", "500", "501", "502", "503", "504", "505", "506", "507", "508", "509", "510", "511", "598", "599"]
+		
+		for code in responseCodes {
+			app.textFields["Response Code"].tap()
+			
+			for number in code {
+				keyDict[String(number)]!.tap()
+			}
+			
+			app.buttons["Search"].tap()
+			// In DetailView Here
+			
+			let detailedDefPlaceholder = app.staticTexts["placeholder"]
+			
+			if homeButton.waitForExistence(timeout: 5) == false {
+				XCTFail()
+			}
+			continueAfterFailure = true
+			if detailedDefPlaceholder.exists {
+				XCTFail("\(code) Contained Detailed Def Placeholder")
+			}
+			continueAfterFailure = false
+			
+			homeButton.tap()
+		}
 	}
 
 	// This test will take an extended period of time
-    func testResponseCodes() throws {
+    func testResponseCodesExist() throws {
         // UI tests must launch the application that they test.
         
 		let app = XCUIApplication()
